4ad0843273081b4e0db80d51ed520e2d45d272b6
sangitalava <sangeeta.lava@yahoo.com>
2026-05-13 12:06:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+
 import "./App.css";
 import "./components/styles/style.css";
 import Board from "./components/Board";
